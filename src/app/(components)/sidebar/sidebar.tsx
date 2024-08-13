559cbcc29b73028dd73f92a66880d210d365a402
IvanRojas FT47b <exequieliv@outlook.es>
2024-08-13 17:50:10 -0300

    seguno2

--- src/app/(components)/sidebar/sidebar.tsx
+++ src/app/(components)/sidebar/sidebar.tsx
@@ -1,9 +1,21 @@
+"use client"
+import { Menu } from "lucide-react"
 
 
 const SideBar = () => {
   return (
     <div>
-        <div className="flex gap-3 justify-between">
+        {/* top  */}
+        <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+            <div>Logo</div>
+            <h1 className="font-extrabold text-2xl">Dashboard</h1>
+       
+        <button className="md:hidden  px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100">
+            <Menu className="w-4 h-4"/>
+        </button>
+        </div>
+        {/* links */}
+        <div className="flex-grow mt-8 ">
 
         </div>
     </div>
