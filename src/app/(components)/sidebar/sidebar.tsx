cf1bf412678e892f4cae739da6a36b2f138ac980
IvanRojas FT47b <exequieliv@outlook.es>
2024-08-16 04:16:13 -0300

    a

--- src/app/(components)/sidebar/sidebar.tsx
+++ src/app/(components)/sidebar/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/state";
-import { Layout, LucideIcon, Menu } from "lucide-react";
+import { Archive, Clipboard, Layout, LucideIcon, Menu } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -27,10 +27,16 @@ const SidebarLink = ({
         className={`cursor-pointer flex items-center ${
           isColapsed ? "justify-center py-4" : "justify-start px-8 py-4"
         }
-          hover:text-blue-500 hover:bg-blue-100 transition-colors ${isActive ? "bg-blue-200 text-white" : ""} ` }
+          hover:text-blue-500 hover:bg-blue-100 transition-colors ${
+            isActive ? "bg-blue-200 text-white" : ""
+          } `}
       >
         <Icon className="w-6 h-6 !text-gray-700" />
-        <span className={`${isColapsed ? "hidden" : "block"} font-medium text-gray-700`}>
+        <span
+          className={`${
+            isColapsed ? "hidden" : "block"
+          } font-medium text-gray-700`}
+        >
           {label}
         </span>
       </div>
@@ -77,7 +83,36 @@ const SideBar = () => {
       </div>
       {/* links */}
       <div className="flex-grow mt-8 ">
-        <SidebarLink href="/dashboard" icon={Layout} label="dashboard" isColapsed={isSideBarCollapsed}/>
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="dashboard"
+          isColapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/inventory"
+          icon={Archive}
+          label="Inventory"
+          isColapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/products"
+          icon={Clipboard}
+          label="Products"
+          isColapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="dashboard"
+          isColapsed={isSideBarCollapsed}
+        />
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="dashboard"
+          isColapsed={isSideBarCollapsed}
+        />
       </div>
       {/* footer */}
       <div>
